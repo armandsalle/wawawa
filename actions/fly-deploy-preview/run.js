c0cc4d86ca6a884702900081ccd8aecd1867a052
Armand SALLE <armand.salle@gmail.com>
2024-08-11 19:17:39 +0200

    feat: Add route for root endpoint to return "OK" (#1)
    * feat: Add route for root endpoint to return "OK"
    
    * chore: Add Fly Deploy Preview API workflow
    
    * chore: Update FLY_REGION to "cdg" in fly-preview.yml
    
    * chore: Update FLY_API_TOKEN in fly-preview.yml to use FLY_AUTH_TOKEN secret
    
    * chore: Update FLY_API_TOKEN and FLY_REGION in fly-preview.yml
    
    * chore: Update FLY_API_TOKEN and FLY_REGION in fly-preview.yml
    
    * chore: Update fly-preview.yml to use fly-deploy-preview instead of fly-preview-apps
    
    * chore: Update fly-deploy-preview configuration in run.js
    
    * chore: Update fly-deploy-preview configuration in run.js
    
    * chore: Update fly-deploy-preview configuration in run.js
    
    * chore: Update fly-deploy-preview configuration in run.js
    
    * chore: Update fly-preview.yml to use fly-deploy-preview instead of fly-preview-apps

---
+++ actions/fly-deploy-preview/run.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env zx
+
+(async () => {
+  const appName = argv.appName;
+  if (!appName) throw new Error("need --appName");
+
+  const appsStr = await $`flyctl apps list -j`.quiet();
+  const apps = JSON.parse(appsStr);
+  const alreadyExists = apps.some((it) => it.Name === appName);
+
+  if (!alreadyExists) {
+    await $`flyctl launch \\
+        --config "$(pwd)/api/fly.toml" \\
+        --name=${appName} \\
+        --no-deploy \\
+        --ha=false \\
+        --region=cdg \\
+        --yes
+      `;
+  }
+
+  // update secrets
+  const secrets = process.env.INPUT_SECRETS.replaceAll("\n", " ").split(" ");
+  fs.writeFileSync(
+    "secrets.txt",
+    ["NODE_ENV=production", `PREVIEW_NAME=${appName}`, ...secrets].join("\n"),
+  );
+  await $`cat secrets.txt | flyctl secrets import --app ${appName}`;
+  // build and deploy
+  await $`flyctl deploy \\
+      --app ${appName} \\
+      --ha=false \\
+      --remote-only \\
+      --config "$(pwd)/api/fly.toml" \\
+      --dockerfile "$(pwd)/api/Dockerfile"`;
+
+  console.log(alreadyExists);
+})();
